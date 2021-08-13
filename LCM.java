badab4800f0161e7ab0dc47cc71fbdb52345ea54
sanjsamanta <88873140+sanjsamanta@users.noreply.github.com>
2021-08-13 11:16:30 +0530

    Update LCM.java

--- LCM.java
+++ LCM.java
@@ -1,1 +1,17 @@
 
+public class Lcm  
+{  
+  public static void main(String args[])   
+  {  
+    int a = 12, b = 9, gcd = 1;  
+ 
+    for(int i = 1; i <= a && i <= b; ++i)   
+    {   
+      if(a % i == 0 && b % i == 0)  
+      gcd = i;  
+    }  
+    int lcm = (a * b) / gcd;  
+ 
+    System.out.printf("The LCM of %d and %d is %d.", a, b, lcm);  
+}
+}
