38bf8e37dc0f02b431f89c504292771355c61f3c
sanjsamanta <88873140+sanjsamanta@users.noreply.github.com>
2021-08-13 11:20:31 +0530

    Create FreqNo.java

---
+++ FreqNo.java
@@ -0,0 +1,22 @@
+public class frequency
+{
+  static int frequencyDigits(int n,int d)
+  {
+    int c = 0;
+    while (n > 0)
+    {
+        if (n % 10 == d)
+            c++;
+        n = n / 10;
+    }
+    return c;
+  }
+ 
+public static void main(String args[])
+{
+    int N = 1122322;
+    int D = 2;
+    System.out.println(frequencyDigits(N, D));
+}
+}
+ 
